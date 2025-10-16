cc81982760e342cb236cfae169d605933e0541c3
Blacxs23 <thitiwatcatb002244@gmail.com>
2025-10-16 21:02:54 +0700

    forgot to del spacebar

--- Lab-09_01_34.py
+++ Lab-09_01_34.py
@@ -2,10 +2,9 @@
 #6809700039
 #34
 
-while True:
-    num1,num2 = map(int,input().split())
+while True:  #infinity loop
+    num1,num2 = map(int,input().split())     
     answer = num1 + num2
     if num1 == 0 and num2 == 0:
         break
     print(f"{num1} + {num2} = {answer}")
-    
