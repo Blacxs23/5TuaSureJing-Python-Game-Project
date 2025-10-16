510e22a0682b3ea09a9c82a09832c3c404ca572e
Blacxs23 <thitiwatcatb002244@gmail.com>
2025-10-16 23:45:51 +0700

    finish add comments in no 5

--- Lab-09_05_34.py
+++ Lab-09_05_34.py
@@ -2,14 +2,14 @@
 #6809700039
 #34
 
-base = str(input())
-alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-digit = "1234567890"
-answer1 = ""
-answer2 = ""
-for i in base:
-    if i in alphabet:
-        answer1 += i
-    elif i in digit:
-        answer2 += i    
-print(f"Alphabet: {answer1}\nDecimal digit: {answer2}")
+base = str(input()) #input string
+alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" # make string to compare
+digit = "1234567890" # make string to compare 
+answer1 = "" #make string. variable
+answer2 = ""#make string variable
+for i in base: # loop string in base
+    if i in alphabet: # if i is alphabet 
+        answer1 += i #add that character to answer1 variable
+    elif i in digit: # if i is digit
+        answer2 += i # add that character to answer2 variable    
+print(f"Alphabet: {answer1}\nDecimal digit: {answer2}") #output result
