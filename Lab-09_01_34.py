6a5de7a0a7474a5b4918797dacf3f663dcbe4e77
Blacxs23 <thitiwatcatb002244@gmail.com>
2025-10-16 19:19:35 +0700

    code work ready to merge

--- Lab-09_01_34.py
+++ Lab-09_01_34.py
@@ -2,8 +2,10 @@
 #6809700039
 #34
 
-num1,num2 = input().split()
-while num1 and num2 != 0:
+while True:
+    num1,num2 = map(int,input().split())
     answer = num1 + num2
+    if num1 == 0 and num2 == 0:
+        break
     print(f"{num1} + {num2} = {answer}")
     
