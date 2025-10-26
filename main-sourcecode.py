c1050c35121398eae362c8958c9b1826f989ba9c
Blacxs23 <thitiwatcatb002244@gmail.com>
2025-10-26 15:12:58 +0700

    fix cant quit

--- main-sourcecode.py
+++ main-sourcecode.py
@@ -6,6 +6,6 @@ screen = pygame.display.set_mode((1280,720))
 running = True
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.quit:
+        if event.type == pygame.QUIT:
             running = False
-pygame.quit
+pygame.QUIT
