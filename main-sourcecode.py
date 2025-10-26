6334275085f60eb973f600cde813313fee87042e
Blacxs23 <thitiwatcatb002244@gmail.com>
2025-10-26 14:26:01 +0700

    create game window

--- main-sourcecode.py
+++ main-sourcecode.py
@@ -1,2 +1,11 @@
 import pygame
+pygame.init()
+pygame.display.set_caption("Tug Of War")
+screen = pygame.display.set_mode((1280,720))
 
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.quit:
+            running = False
+pygame.quit
