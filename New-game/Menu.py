5d671704fd0955b8e082829cbf8d2572963c5bb4
SeatthaMat <seatthatanachot.mat@dome.tu.ac.th>
2025-11-05 22:09:28 +0700

    add bg

--- New-game/Menu.py
+++ New-game/Menu.py
@@ -7,14 +7,14 @@ pygame.init()
 screen_w = 1024
 screen_h = 576
 screen = pygame.display.set_mode((screen_w,screen_h))
-pygame.display.set_caption("Tug of War")
+pygame.display.set_caption("DTI Fighter")
 
 # background 1
-bg_start = pygame.image.load("Tug of War/bg/tow_bg1.jpg")
+bg_start = pygame.image.load("New-game/asset/bg/bg1_dome.png")
 screen.blit(bg_start,(0,0))
 
 # logo
-logo = pygame.image.load("Tug of War/logo/tow_logo.png")
+logo = pygame.image.load("New-game/asset/logo/tow_logo.png")
 logo = pygame.transform.scale(logo,(502,278))
 logo_rect = logo.get_rect()
 logo_rect.centerx = screen_w // 2
@@ -24,7 +24,7 @@ screen.blit(logo,logo_rect)
 # ------play button------
 
 # play default
-play_default = pygame.image.load("Tug of War/button/play_default.png")
+play_default = pygame.image.load("New-game/asset/button/play_default.png")
 play_default = pygame.transform.scale(play_default,(225,45))
 play_default_rect = play_default.get_rect()
 play_default_rect.centerx = screen_w // 2
@@ -32,7 +32,7 @@ play_default_rect.centery = 300
 # screen.blit(play_default,play_default_rect)
 
 #  play click
-play_click = pygame.image.load("Tug of War/button/play_click.png")
+play_click = pygame.image.load("New-game/asset/button/play_click.png")
 play_click = pygame.transform.scale(play_click,(225,45))
 play_click_rect = play_click.get_rect()
 play_click_rect.centerx = screen_w // 2
@@ -42,7 +42,7 @@ play_click_rect.centery = 300
 # ------control button------
 
 # control_default
-control_default = pygame.image.load("Tug of War/button/control_default.png")
+control_default = pygame.image.load("New-game/asset/button/control_default.png")
 control_default = pygame.transform.scale(control_default,(225,45))
 control_default_rect = control_default.get_rect()
 control_default_rect.centerx = screen_w // 2
@@ -50,7 +50,7 @@ control_default_rect.centery = 350
 # screen.blit(control_default,control_default_rect)
 
 # control_click
-control_click = pygame.image.load("Tug of War/button/control_click.png")
+control_click = pygame.image.load("New-game/asset/button/control_click.png")
 control_click = pygame.transform.scale(control_click,(225,45))
 control_click_rect = control_click.get_rect()
 control_click_rect.centerx = screen_w // 2
@@ -60,7 +60,7 @@ control_click_rect.centery = 350
 # -----credit button-----
 
 # credit_default
-credit_default = pygame.image.load("Tug of War/button/credit_default.png")
+credit_default = pygame.image.load("New-game/asset/button/credit_default.png")
 credit_default = pygame.transform.scale(credit_default,(160,32))
 credit_default_rect = credit_default.get_rect()
 credit_default_rect.centerx = screen_w // 2
@@ -68,7 +68,7 @@ credit_default_rect.centery = 400
 # screen.blit(credit_default,credit_default_rect)
 
 # credit_click
-credit_click = pygame.image.load("Tug of War/button/credit_click.png")
+credit_click = pygame.image.load("New-game/asset/button/credit_click.png")
 credit_click = pygame.transform.scale(credit_click,(160,32))
 credit_click_rect = credit_click.get_rect()
 credit_click_rect.centerx = screen_w // 2
@@ -78,11 +78,11 @@ credit_click_rect.centery = 400
 # ----- popup -----
 
 # control_popup
-control_popup = pygame.image.load("Tug of War/asset/popup/control_popup.png")
-control_popup = pygame.transform.scale(control_popup,(500,600))
-control_popup_rect = control_popup.get_rect()
-control_popup_rect.centerx = screen_w // 2
-control_popup_rect.centery = 290
+# control_popup = pygame.image.load(" ")
+# control_popup = pygame.transform.scale(control_popup,(500,600))
+# control_popup_rect = control_popup.get_rect()
+# control_popup_rect.centerx = screen_w // 2
+# control_popup_rect.centery = 290
 # screen.blit(control_popup,control_popup_rect)
 
 current_play = "play"
