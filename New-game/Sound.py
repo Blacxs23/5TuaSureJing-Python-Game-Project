2ae32c25fab7088353b8186f49aea5f7eddae8fb
moonknight188 <nmike5555@gmail.com>
2025-11-06 02:59:27 +0700

    Update Sound.py

--- New-game/Sound.py
+++ New-game/Sound.py
@@ -4,6 +4,6 @@ from pygame import mixer
 mixer.init()
 
 #load music snd sonds
-pygame.mixer.music.load("asset/sound/music1.mp3")
+pygame.mixer.music.load("New-game/asset/sound/music1.mp3")
 pygame.mixer.music.set_volume(0.5)
 pygame.mixer.music.play(-1, 0.0, 5000)
