c4d6f0ece2ae88f017c28aa5a9f1a327b15c69ae
judasgn <chatmongkol.mue@dome.tu.ac.th>
2025-11-06 02:15:09 +0700

    Update Character.py

--- New-game/Character.py
+++ New-game/Character.py
@@ -10,7 +10,7 @@ class Fighter():
         self.attack_type = 0  
 
     # move function
-    def move(self, screen_width):
+    def move(self, screen_width, screen_hight, surface):
         speed = 10
         dx = 0
         dy = 0
@@ -26,6 +26,7 @@ class Fighter():
 
         #attack
         if key[pygame.K_f] or key[pygame.K_0]:
+          self.attack(surface)
 
           #attack type was used
           if key[pygame.K_f]:
@@ -43,9 +44,9 @@ class Fighter():
         self.rect.x += dx
         self.rect.y += dy
 
-def attack(self):
+def attack(self, surface):
     attack_rect = pygame.Rect(self.rect.centrex, self.rect.y, 2 * self.rect.width, self.rect.height)
-    
+    pygame.draw.rect(surface, (0, 255, 0), self.rect)
 
     def draw(self, surface):
         pygame.draw.rect(surface, (255, 0, 0), self.rect)
