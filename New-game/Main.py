b9557aa5f397ee688a1a56e4699fa39f52eae07f
709werawat@gmail.com <709werawat@gmail.com>
2025-11-05 15:32:34 +0700

    Update Main.py

--- New-game/Main.py
+++ New-game/Main.py
@@ -13,12 +13,12 @@ clock = pygame.time.Clock()
 FPS = 60
 
 
-#กำหนดสี
+#กำหนดสี  #stamp
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
 
-#ฟังชั่นวาดหลอดเลือด
+#ฟังชั่นวาดหลอดเลือด  #stamp
 def draw_health_bar(health, x, y):
     pygame.draw.rect(screen, YELLOW, (x, y, 400, 30))
 
@@ -34,15 +34,6 @@ def draw_bg():
     scaled_bg = pygame.transform.scale(bg_image, (screen_width, screen_height))
     screen.blit(scaled_bg, (0,0))
 
-
-#กำหนดสี
-RED = (255, 0, 0)
-YELLOW = (255, 255, 0)
-WHITE = (255, 255, 255)
-
-
-
-
 #game loop
 run = True
 while run: #all running game code must in this while loop
@@ -53,7 +44,7 @@ while run: #all running game code must in this while loop
     #draw bg
     draw_bg()
 
-    #แสดงหลอดเลือด
+    #แสดงหลอดเลือด   #stamp
     draw_health_bar(fighter_1.health, 30, 20)
     draw_health_bar(fighter_2.health, 850, 20)
     
