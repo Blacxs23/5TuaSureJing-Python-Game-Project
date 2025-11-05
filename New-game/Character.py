d9b040cf9efe81d173576195a535a9dd524dbdff
judasgn <chatmongkol.mue@dome.tu.ac.th>
2025-11-06 02:04:04 +0700

    Update Character.py

--- New-game/Character.py
+++ New-game/Character.py
@@ -7,6 +7,7 @@ class Fighter():
     def __init__(self, x, y):
         self.rect = pygame.Rect((x,y, 80,180))
         self.health = 100
+        self.attack_type = 0  
 
     # move function
     def move(self, screen_width):
@@ -22,6 +23,15 @@ class Fighter():
             dx = -speed
         if key[pygame.K_d]:
             dx = speed
+
+        #attack
+        if key[pygame.K_f] or key[pygame.K_0]:
+
+          #attack type was used
+          if key[pygame.K_f]:
+              self.attack_type = 1
+          if key[pygame.k_0]:
+              self.attack_type = 2
 
         #ensure player stays on screen
         if self.rect.left + dx < 0:
