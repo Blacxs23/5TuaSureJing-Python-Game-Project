e9898ea99c0837281b2e84089362a6a238463bd3
moonknight188 <nmike5555@gmail.com>
2025-11-06 09:45:00 +0700

    Update Sound.py

--- New-game/Sound.py
+++ New-game/Sound.py
@@ -3,7 +3,33 @@ from pygame import mixer
 
 mixer.init()
 
-#load music snd sonds
+#load music and set volume
 pygame.mixer.music.load("New-game/asset/sound/music1.mp3")
 pygame.mixer.music.set_volume(0.5)
 pygame.mixer.music.play(-1, 0.0, 5000)
+
+pygame.mixer.music.load("New-game/asset/sound/music2.mp3")
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1, 0.0, 5000)
+
+pygame.mixer.music.load("New-game/asset/sound/music3.mp3")
+pygame.mixer.music.set_volume(0.5) 
+pygame.mixer.music.play(-1, 0.0, 5000)
+
+#play attack and take hit sounds
+attack1_sound = mixer.Sound("New-game/asset/sound/attack1.wav")
+attack1_sound.set_volume(0.3)
+attack2_sound = mixer.Sound("New-game/asset/sound/attack2.wav")
+attack2_sound.set_volume(0.3)
+
+takehit1_sound = mixer.Sound("New-game/asset/sound/takehit.wav")
+takehit1_sound.set_volume(0.3)
+takehit2_sound = mixer.Sound("New-game/asset/sound/takehit2.wav")
+takehit2_sound.set_volume(0.3)
+
+dead_sound1 = mixer.Sound("New-game/asset/sound/dead1.wav")
+dead_sound1.set_volume(0.3)
+dead_sound2 = mixer.Sound("New-game/asset/sound/dead2.wav")
+dead_sound2.set_volume(0.3)
+
+
