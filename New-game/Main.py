ca9e9ac0ac5c0fd8bd067c99536b6d18d388cb03
709werawat@gmail.com <709werawat@gmail.com>
2025-11-06 10:02:30 +0700

    comit รอบที่ล้าน

--- New-game/Main.py
+++ New-game/Main.py
@@ -20,7 +20,10 @@ def main_game(screen):
 
     #ฟังชั่นวาดหลอดเลือด  #stamp
     def draw_health_bar(health, x, y):
-        pygame.draw.rect(screen, YELLOW, (x, y, 400, 30))
+       ratio = health / 100
+    pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 404, 34))
+    pygame.draw.rect(screen, RED, (x, y, 400, 30))
+    pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
 
     #import background image
     bg_image = pygame.image.load("New-game/asset/bg/bg2_boonchoo.png")
@@ -56,8 +59,8 @@ def main_game(screen):
         draw_bg()
 
         #แสดงหลอดเลือด   #stamp
-        # draw_health_bar(fighter_1.health, 30, 20)
-        # draw_health_bar(fighter_2.health, 600, 20)
+        draw_health_bar(fighter_1.health, 30, 20)
+        draw_health_bar(fighter_2.health, 600, 20)
         
         #move fighter
         fighter_1.move(screen_width, screen_height, screen)
